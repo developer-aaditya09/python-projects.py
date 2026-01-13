df661479587fe1c4d255441f5e5665b0f3ee6eec
Aaditya Singh <www.aadityasingh2009@gmail.com>
2026-01-13 22:58:48 +0530

    Create agedetector.py
    it is a code for beignner to know about how if and else works.

---
+++ agedetector.py
@@ -0,0 +1,11 @@
+print("hey user welcome to our system")
+
+age=int(input("enter your age:"))
+
+if age>=18:
+
+    print("you are a adult person you can consume our content")
+
+else:
+
+    print("you are a minor")
